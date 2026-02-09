92082acd2628c7d9af5bb97578d90c4ac1d2cf4e
breadbread1984 <breadbread1984@163.com>
2026-02-09 16:16:57 +0800

    update

--- docker/start.sh
+++ docker/start.sh
@@ -17,24 +17,5 @@ xpra start :100 \
 
 XPRAPID=$!
 
-# 3) launch sandbox
-# 3.1) launch browser sandbox
-echo "3) starting sandbox browser service ..."
-cd /app/sandbox/browser
-python3 -m pip install -r requirements.txt
-playwright install
-python3 main.py --service_host 0.0.0.0 --service_port 8081 &
-BROWSER_SANDBOX_PID=$!
-
-# 4) launch nginx
-echo "4) starting nginx service ..."
-ln -s /app/index.html /var/www/html/
-ln -s /app/sandbox.com /etc/nginx/sites-available/
-ln -s /app/sandbox.com /etc/nginx/sites-enabled/
-rm -f /etc/nginx/sites-enabled/default
-
-nginx -g "daemon off;" &
-NGINX_PID=$!
-
 # 5) block container
-wait $SSH_PID $XPRAPID $BROWSER_SANDBOX_PID $NGINX_PID
+wait $SSH_PID $XPRAPID
--- tools/browser/tool.py
+++ tools/browser/tool.py
@@ -1,87 +1,35 @@
 #!/usr/bin/python3
 
+import asyncio
 from typing import Type, List, Optional, Annotated, Literal, Union
 from pydantic import BaseModel, Field, validator, root_validator
+from langchain_openai import ChatOpenAI
 from langchain_core.tools.structured import StructuredTool
+from langgraph.prebuilt import InjectedState
+from browser_use import Agent
 
-def load_browswer_tool():
-  class NavigateTo(BaseModel):
-    url: str = Field(description = "URL for 'navigate_to' action")
-  class GoBack(BaseModel):
-    pass
-  class Wait(BaseModel):
-    seconds: int = Field(description = "Seconds to wait for load")
-  class ClickElement(BaseModel):
-    index: int = Field(description = "Element index for interaction actions")
-  class InputText(BaseModel):
-    index: int = Field(description = "Element index for interaction actions")
-    text: str = Field(description = "Text for input or scroll actions")
-  class SendKeys(BaseModel):
-    keys: str = Field(description = "Keys to send for keyboard actions")
-  class SwitchTab(BaseModel):
-    page_id: int = Field(description = "Tab ID for tab management actions")
-  class CloseTab(BaseModel):
-    page_id: int = Field(description = "Tab ID for tab management actions")
-  class ScrollDown(BaseModel):
-    amount: int = Field(description = "Pixel amount to scroll")
-  class ScrollUp(BaseModel):
-    amount: int = Field(description = "Pixel amount to scroll")
-  class ScrollToText(BaseModel):
-    text: str = Field(description = "Text for input or scroll actions")
-  class GetDropdownOptions(BaseModel):
-    index: int = Field(description = "Element index for interaction actions")
-  class SelectDropdownOption(BaseModel):
-    index: int = Field(description = "Element index for interaction actions")
-    text: str = Field(description = "Text for input or scroll actions")
-  class ClickCoordinates(BaseModel):
-    x: int = Field(description = "X coordinate for click or drag actions")
-    y: int = Field(description = "Y coordinate for click or drag actions")
-  class DragDrop(BaseModel):
-    element_source: str = Field(description = "Source element for drag and drop")
-    element_target: str = Field(description = "Target element for drag and drop")
-
+def load_browser_tool(configs):
   class BrowserInput(BaseModel):
-    action: Literal["navigate_to", "go_back", "wait", "click_element", "input_text",
-                    "send_keys", "switch_tab", "close_tab", "scroll_down", "scroll_up",
-                    "scroll_to_text", "get_dropdown_options", "select_dropdown_option", "click_coordinates", "drag_drop",] = Field(description = "The browser action to perform")
-    navigate_to: Optional[NavigateTo] = Field(None, description = "parameters for action navigate_to")
-    go_back: Optional[GoBack] = Field(None, description = "parameters for action go_back")
-    wait: Optional[Wait] = Field(None, description = "parameters for action wait")
-    click_element: Optional[ClickElement] = Field(None, description = "paramters for action click_element")
-    input_text: Optional[InputText] = Field(None, description = "parameters for action input_text")
-    send_keys: Optional[SendKeys] = Field(None, description = "parameters for action send_keys")
-    switch_tab: Optional[SwitchTab] = Field(None, description = "parameters for action switch_tab")
-    close_tab: Optional[CloseTab] = Field(None, description = "paramters for action close_tab")
-    scroll_down: Optional[ScrollDown] = Field(None, description = "parameters for action scroll_down")
-    scroll_up: Optional[ScrollUp] = Field(None, description = "parameters for action scroll_up")
-    scroll_to_text: Optional[ScrollToText] = Field(None, description = "parameters for action scroll_to_text")
-    get_dropdown_options: Optional[GetDropdownOptions] = Field(None, description = "parameters for action get_dropdown_options")
-    select_dropdown_option: Optional[SelectDropdownOption] = Field(None, description = "parameters for action select_dropdown_option")
-    click_coordinates: Optional[ClickCoordinates] = Field(None, description = "parameters for action click_coordinates")
-    drag_drop = Optional[DragDrop] = Field(None, description = "parameters for action drag_drop")
+    query: str = Field(description = "a task to assign to a browser agent in natural language")
+  class BrowserOutput(BaseModel):
+    result: str = Field(description = "search result")
+  class BrowserConfig(BaseModel):
+    class Config:
+      arbitrary_types_allowed = True
+    llm: ChatOpenAI
+  class BrowserTool(StructuredTool):
+    name: str = "browser"
+    description: str = "a tool to execute web task. Input should be a natural language task like 'find out which country leads medal table of winter olympic games 2026'."
+    args_schema: Type[BaseModel] = BrowserInput
+    config: BrowserConfig
+    def _arun(self, query: str, state: Annotated[dict, InjectedState], run_manager: Optional[CallbackManagerForToolRun] = None):
+      agent = Agent(
+        task = query,
+        llm = self.config.llm
+      )
+      result = await self.config.agent.run()
+      return BrowserOutput(result = result)
+    def _run(self, query: str, state: Annotated[dict, InjectedState], run_manager: Optional[CallbackManagerForToolRun] = None):
+      return asyncio.run(self._arun(query, state = state, run_manager = run_manager))
+  return BrowserTool(config = BrowserConfig(llm = ChatOpenAI(api_key = configs.dashscope_key, base_url = configs.dashscope_url, model_name = configs.dashscope_model)))
 
-    @root_validator
-    def validate_action_and_parameters(cls, values):
-      action = values.get('action')
-      required_fields = {
-        "navigate_to": "navigate_to",
-        "go_back": "go_back",
-        "wait": "wait",
-        "click_element": "click_element",
-        "input_text": "input_text",
-        "send_keys": "send_keys",
-        "switch_tab": "switch_tab",
-        "close_tab": "close_tab",
-        "scroll_down": "scroll_down",
-        "scroll_up": "scroll_up",
-        "scroll_to_text": "scroll_to_text",
-        "get_dropdown_options": "get_dropdown_options",
-        "select_dropdown_option": "select_dropdown_option",
-        "click_coordinates": "click_coordinates",
-        "drag_drop": "drag_drop",
-      }
-      if action in required_fields:
-        field_name = required_fields[action]
-        if values.get(field_name) is None:
-          raise ValueError(f"When action is '{action}', the field '{field_name}' must not be None.")
-      return values
