18a8581e22bd148763dd71e9d1632bb49fc82e3c
breadbread1984 <breadbread1984@163.com>
2026-02-10 17:40:13 +0800

    update

--- tools/file_management/__init__.py
+++ tools/file_management/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 
-from .tool import load_file_management_tool
+from .tool import load_file_management_tools
--- tools/file_management/tool.py
+++ tools/file_management/tool.py
@@ -4,7 +4,7 @@ from typing import List
 from langchain_core.tools import BaseTool
 from langchain_community.agent_toolkits import FileManagementToolkit
 
-def load_file_management_tool(configs) -> List[BaseTool]:
+def load_file_management_tools(configs) -> List[BaseTool]:
   tools = FileManagementToolkit(
     root_dir = configs.workspace_dir,
     selected_tools = ["copy_file", "file_delete", "file_search", "list_directory", "move_file", "read_file", "write_file"]
