f861f1e6580e8f47575040ec57f8100828dca38a
breadbread1984 <breadbread1984@163.com>
2026-02-10 14:33:25 +0800

    update

---
+++ tests/test_browser.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+from os.path import join, exists, dirname, abspath
+import unittest
+
+sys.path.append(abspath(join(dirname(__file__), '..')))
+
+from tools import load_browser_tool
+import configs
+
+class TestBrowser(unittest.TestCase):
+  def test_browser(self,)
+    browser_tool = load_browser_tool(configs)
+    result = browser_tool.invoke({'query': 'find out what options strategies to apply when market skyrockets up'})
+    print(result)
+
+if __name__ == "__main__":
+  unittest.main()
