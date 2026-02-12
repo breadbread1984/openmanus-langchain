b18f24aa2720956ce5f961c5636f416c22191df8
breadbread1984 <breadbread1984@163.com>
2026-02-12 10:19:32 +0800

    update

--- docker/start.sh
+++ docker/start.sh
@@ -8,16 +8,16 @@ echo "1) starting ssh service ..."
 /usr/sbin/sshd -D &
 
 SSH_PID=$!
-
+'''
 # 2) launch xpra service without desktop
 echo "2) starting xpra service ..."
 xpra start :100 --start-child="/usr/bin/dbus-launch" &
-
 '''
+
 # 2) launch xpra service with desktop
 echo "2) starting xpra service ..."
 xpra start-desktop :100 --start=xfce4-session --dbus-launch=yes &
-'''
+
 
 XPRAPID=$!
 
