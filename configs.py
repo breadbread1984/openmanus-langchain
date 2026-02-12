6d1efc74ef42a5232db1f8eb18337809e008d21c
breadbread1984 <breadbread1984@163.com>
2026-02-12 15:52:26 +0800

    update

--- configs.py
+++ configs.py
@@ -5,4 +5,7 @@ dashscope_vlm_model = 'qwen3-vl-plus'
 dashscope_url = 'https://dashscope.aliyuncs.com/compatible-mode/v1'
 dashscope_key = ''
 
+langsmith_trace = 'true'
+langsmith_api_key = ''
+
 workspace_dir = "./workspace"
